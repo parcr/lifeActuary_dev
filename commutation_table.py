f8a442159c5f93672b88d8d2997744d6381520e9
parcr <pedro.cortereal@magentakoncept.com>
2020-01-23 21:07:31 +0000

    actualization commit

--- commutation_table.py
+++ commutation_table.py
@@ -43,7 +43,6 @@ class CommutationFunctions(MortalityTable):
         :param n: years until payment, if x is alive
         :return: the present value of a pure endowment of 1 at age x+n
         """
-        self.msn.append(f"{n}_E_{x}")
         if x < 0:
             return np.nan
         if n <= 0:
@@ -52,7 +51,7 @@ class CommutationFunctions(MortalityTable):
             return 0.
         D_x = self.Dx[x]
         D_x_n = self.Dx[x + n]
-        self.msn[-1] = f"{n}_E_{x}={D_x_n} / {D_x}"
+        self.msn.append(f"{n}_E_{x}={D_x_n} / {D_x}")
         return D_x_n / D_x
 
     def Ax(self, x):
@@ -62,7 +61,6 @@ class CommutationFunctions(MortalityTable):
         :return: Expected Present Value (EPV) of a whole life insurance (i.e. net single premium), that pays 1,at the
         end of the year of death. It is also commonly referred to as the Actuarial Value or Actuarial Present Value.
         """
-        self.msn.append(f"A_{x}")
         if x < 0:
             return np.nan
         if x > self.w:
@@ -82,7 +80,6 @@ class CommutationFunctions(MortalityTable):
         :return: Expected Present Value (EPV) of a whole life insurance (i.e. net single premium), that pays 1, at the
         moment of death. It is also commonly referred to as the Actuarial Value or Actuarial Present Value.
         """
-        self.msn.append(f"A_{x}_")
         if x < 0:
             return np.nan
         if x > self.w:  # it will die before year's end, because already attained age>w
@@ -104,7 +101,6 @@ class CommutationFunctions(MortalityTable):
         pays 1, at the end of the year of death. It is also commonly referred to as the Actuarial Value or
         Actuarial Present Value.
         """
-        self.msn.append(f"{n}_A_{x}")
         if x < 0:
             return np.nan
         if n < 0:
@@ -130,7 +126,6 @@ class CommutationFunctions(MortalityTable):
         pays 1, at the moment of death. It is also commonly referred to as the Actuarial Value or
         Actuarial Present Value.
         """
-        self.msn.append(f"{n}_A_{x}_")
         if x < 0:
             return np.nan
         if n < 0:
@@ -146,3 +141,27 @@ class CommutationFunctions(MortalityTable):
             M_x_n = self.Mx[x + n] * self.cont
         self.msn.append(f"{n}_A_{x}_=({M_x}-{M_x_n}) / {D_x}")
         return (M_x - M_x_n) / D_x
+
+    def nAEx(self, x, n):
+        """
+        Endowment insurance
+        :param x: age at the beginning of the contract
+        :param n: period of the contract
+        :return: Expected Present Value (EPV) of an Endowment life insurance (i.e. net single premium), that
+        pays 1, at the end of year of death or 1 if x survives to age x+n. It is also commonly referred to as the
+        Actuarial Value or Actuarial Present Value.
+        """
+        self.msn.append(f"{n}_AE_{x}={n}_A_{x}+{n}_E_{x}")
+        return self.nAx(x, n) + self.nEx(x, n)
+
+    def nAEx_(self, x, n):
+        """
+        Endowment insurance
+        :param x: age at the beginning of the contract
+        :param n: period of the contract
+        :return: Expected Present Value (EPV) of an Endowment life insurance (i.e. net single premium), that
+        pays 1, at the moment of death or 1 if x survives to age x+n. It is also commonly referred to as the
+        Actuarial Value or Actuarial Present Value.
+        """
+        self.msn.append(f"{n}_AE_{x}_={n}_A_{x}_+{n}_E_{x}")
+        return self.nAx_(x, n) + self.nEx(x, n)
