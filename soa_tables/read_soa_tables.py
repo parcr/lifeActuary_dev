3d5ee68f692fcc22ca06f0bd57bf0fc2df0cf4a4
parcr <pedro.cortereal@magentakoncept.com>
2020-02-01 19:21:46 +0000

    actualization commit

--- soa_tables/read_soa_tables.py
+++ soa_tables/read_soa_tables.py
@@ -1,7 +1,25 @@
 from xml.dom import minidom
-# xmldoc = minidom.parse('t1.xml')
-xmldoc = minidom.parse('t34059.xml')
-itemlist = xmldoc.getElementsByTagName('Y')
 
-for s in itemlist:
-    print(f"{s.attributes['t'].value}: {s.childNodes[0].data}")
+http_header = 'https://mort.soa.org/ViewTable.aspx?&TableIdentity='
+xmldoc = minidom.parse('t1.xml')
+xmldoc = minidom.parse('t34059.xml')
+# xmldoc = minidom.parse('t954.xml')
+table_id = xmldoc.getElementsByTagName('TableIdentity')[0].childNodes[0].data
+url = http_header + table_id
+
+name = xmldoc.getElementsByTagName('TableName')[0].childNodes[0].data
+contentType = xmldoc.getElementsByTagName('ContentType')[0].childNodes[0].data
+tableReference = xmldoc.getElementsByTagName('TableReference')[0].childNodes[0].data
+ages = xmldoc.getElementsByTagName('Y')
+
+min_age = int(ages[0].attributes['t'].value)
+max_age = min_age + len(ages) - 1
+
+print(f"Preparing table {name} with minimum age {min_age} and maximum age {max_age}\n{url}")
+table_qx = [float(age.childNodes[0].data) for age in ages]
+table_qx.insert(0, min_age)
+
+'''
+for age in ages:
+    print(f"{age.attributes['t'].value}: {age.childNodes[0].data}")
+'''
