b27e15478355c5ee7197d46c9c47376b4e5b0ccb
parcr <pedro.cortereal@magentakoncept.com>
2020-02-03 16:43:02 +0000

    take out the 1000 divisor from the qx

--- essay/essay_life_table_2.py
+++ essay/essay_life_table_2.py
@@ -7,7 +7,7 @@ mt_TV7377 = rst.SoaTable('../soa_tables/TV7377.xml')
 mt_GKF95 = rst.SoaTable('../soa_tables/GRF95.xml')
 
 lt_tv7377 = mortality_table.MortalityTable(mt=mt_TV7377.table_qx)
-lt_gkm95 = mortality_table.MortalityTable(mt=mt_GKF95)
+lt_gkm95 = mortality_table.MortalityTable(mt=mt_GKF95.table_qx)
 cf_tv7377 = commutation_table.CommutationFunctions(i=2, g=1.5, mt=mt_TV7377.table_qx)
 print(cf_tv7377.df_commutation_table())
 print(cf_tv7377.aax(73), cf_tv7377.msn[-1])
