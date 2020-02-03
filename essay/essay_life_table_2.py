24cdddbfa78af972c1f36a64a3d2890e85bbea63
parcr <pedro.cortereal@magentakoncept.com>
2020-02-03 16:30:37 +0000

    actualization commit

--- essay/essay_life_table_2.py
+++ essay/essay_life_table_2.py
@@ -1,10 +1,13 @@
 # author: PedroCR #
-from toDelete.mortality_tables_old import TV7377, GKM95_lx_15
+from soa_tables import read_soa_table_xml as rst
 import mortality_table
 import commutation_table
 
-lt_tv7377 = mortality_table.MortalityTable(mt=TV7377)
-lt_gkm95 = mortality_table.MortalityTable(data_type='l', mt=GKM95_lx_15)
-cf_tv7377 = commutation_table.CommutationFunctions(i=2, g=1.5, mt=TV7377)
+mt_TV7377 = rst.SoaTable('../soa_tables/TV7377.xml')
+mt_GKF95 = rst.SoaTable('../soa_tables/GRF95.xml')
+
+lt_tv7377 = mortality_table.MortalityTable(mt=mt_TV7377.table_qx)
+lt_gkm95 = mortality_table.MortalityTable(mt=mt_GKF95)
+cf_tv7377 = commutation_table.CommutationFunctions(i=2, g=1.5, mt=mt_TV7377.table_qx)
 print(cf_tv7377.df_commutation_table())
 print(cf_tv7377.aax(73), cf_tv7377.msn[-1])
