1378f3d9334550416e7cee924be0313bd9199c11
parcr <pedro.cortereal@magentakoncept.com>
2020-02-04 10:51:41 +0000

    start to implement the new module annuities.py

--- annuities.py
+++ annuities.py
@@ -3,7 +3,67 @@ __author__ = "PedroCR"
 import numpy as np
 
 
-# life annuities
+# life annuities 1 head
+def ax(mtx, x, i=None, g=0, m=1, defer=0, method='udd'):
+    '''
+    computes a whole life annuity immediate paying while both lives are alive
+    :param mtx: table for life x
+    :param x: age x
+    :param i: technical interest rate (flat rate) in percentage, e.g., 2 for 2%
+    :param g: growth rate (flat rate) in percentage, e.g., 2 for 2%
+    :param m: frequency of payments per unit of interest rate quoted
+    :param defer: deferment period
+    :param method: the method to approximate the fractional periods
+    :return: the actuarial present value
+    '''
+    years_to_wx = mtx.w - (x + defer)
+    years_to_end = np.max(years_to_wx, 0)
+    i = i / 100
+    g = g / 100
+    d = float((1 + g) / (1 + i))
+    if years_to_end == 0:  # at least one will die before the end of the period
+        return .0
+    number_of_payments = int(years_to_end * m)
+    # starts paying one period after the deferment
+    payments_instants = np.linspace(defer + 1 / m, years_to_end, number_of_payments)
+    instalments = [mtx.tpx(x, t=t, method=method) *
+                   np.power(d, t) for t in payments_instants]
+    instalments = np.array(instalments) / np.power(1 + g, defer) / m
+
+    return np.sum(instalments)
+
+
+def aax(mtx, x, i=None, g=0, m=1, defer=0, method='udd'):
+    '''
+    computes a whole life annuity due paying while both lives are alive
+    :param mtx: table for life x
+    :param x: age x
+    :param i: technical interest rate (flat rate) in percentage, e.g., 2 for 2%
+    :param g: growth rate (flat rate) in percentage, e.g., 2 for 2%
+    :param m: frequency of payments per unit of interest rate quoted
+    :param defer: deferment period
+    :param method: the method to approximate the fractional periods
+    :return: the actuarial present value
+    '''
+
+    years_to_wx = mtx.w - (x + defer)
+    years_to_end = np.max(years_to_wx, 0)
+    i = i / 100
+    g = g / 100
+    d = float((1 + g) / (1 + i))
+    if years_to_end == 0:  # at least one will die before the end of the period
+        return .0
+    number_of_payments = int(years_to_end * m)
+    # starts paying immediately after the deferment
+    payments_instants = np.linspace(defer + 1 / m * 0, years_to_end, number_of_payments)
+    instalments = [mtx.tpx(x, t=t, method=method) *
+                   np.power(d, t) for t in payments_instants]
+    instalments = np.array(instalments) / np.power(1 + g, defer) / m
+
+    return np.sum(instalments)
+
+
+# life annuities 2 heads
 def axy(mtx, mty, x, y, i=None, g=0, m=1, defer=0, method='udd'):
     '''
     computes a whole life annuity immediate paying while both lives are alive
@@ -27,6 +87,7 @@ def axy(mtx, mty, x, y, i=None, g=0, m=1, defer=0, method='udd'):
     if years_to_end == 0:  # at least one will die before the end of the period
         return .0
     number_of_payments = int(years_to_end * m)
+    # starts paying one period after the deferment
     payments_instants = np.linspace(defer + 1 / m, years_to_end, number_of_payments)
     instalments = [mtx.tpx(x, t=t, method=method) * mty.tpx(y, t=t, method=method) *
                    np.power(d, t) for t in payments_instants]
@@ -50,4 +111,19 @@ def aaxy(mtx, mty, x, y, i=None, g=0, m=1, defer=0, method='udd'):
     :return: the actuarial present value
     '''
 
-    return 1 / m / np.power(1 + g, defer) + axy(mtx, mty, x, y, i, g, m, defer, method)
+    years_to_wx = mtx.w - (x + defer)
+    years_to_wy = mty.w - (y + defer)
+    years_to_end = np.max(np.min((years_to_wx, years_to_wy)), 0)
+    i = i / 100
+    g = g / 100
+    d = float((1 + g) / (1 + i))
+    if years_to_end == 0:  # at least one will die before the end of the period
+        return .0
+    number_of_payments = int(years_to_end * m)
+    # starts paying immediately after the deferment
+    payments_instants = np.linspace(defer + 1 / m * 0, years_to_end, number_of_payments)
+    instalments = [mtx.tpx(x, t=t, method=method) * mty.tpx(y, t=t, method=method) *
+                   np.power(d, t) for t in payments_instants]
+    instalments = np.array(instalments) / np.power(1 + g, defer) / m
+
+    return np.sum(instalments)
