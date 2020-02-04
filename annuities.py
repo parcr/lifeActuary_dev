5d333c683bc69103c655f5df143011ab272c5bc4
parcr <pedro.cortereal@magentakoncept.com>
2020-02-04 10:21:33 +0000

    start to implement the new module annuities.py

--- annuities.py
+++ annuities.py
@@ -26,7 +26,7 @@ def axy(mtx, mty, x, y, i=None, g=0, m=1, defer=0, method='udd'):
     d = float((1 + g) / (1 + i))
     if years_to_end == 0:  # at least one will die before the end of the period
         return .0
-    number_of_payments = int(years_to_end * m) # todo: confirm number of payments
+    number_of_payments = int(years_to_end * m)
     payments_instants = np.linspace(defer + 1 / m, years_to_end, number_of_payments)
     instalments = [mtx.tpx(x, t=t, method=method) * mty.tpx(y, t=t, method=method) *
                    np.power(d, t) for t in payments_instants]
@@ -35,7 +35,7 @@ def axy(mtx, mty, x, y, i=None, g=0, m=1, defer=0, method='udd'):
     return np.sum(instalments)
 
 
-def aaxy(mtx, mty, x, y, i=None, g=0, m=1):
+def aaxy(mtx, mty, x, y, i=None, g=0, m=1, defer=0, method='udd'):
     '''
     computes a whole life annuity due paying while both lives are alive
     :param mtx: table for life x
@@ -45,7 +45,9 @@ def aaxy(mtx, mty, x, y, i=None, g=0, m=1):
     :param i: technical interest rate (flat rate) in percentage, e.g., 2 for 2%
     :param g: growth rate (flat rate) in percentage, e.g., 2 for 2%
     :param m: frequency of payments per unit of interest rate quoted
+    :param defer: deferment period
+    :param method: the method to approximate the fractional periods
     :return: the actuarial present value
     '''
 
-    return 1 + axy(mtx, mty, x, y, i, g, m) + 1 / m
+    return 1 / m / np.power(1 + g, defer) + axy(mtx, mty, x, y, i, g, m, defer, method)
