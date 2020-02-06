1f55ba96c593247eba19501b86a11b79e72e5414
parcr <pedro.cortereal@magentakoncept.com>
2020-02-06 15:39:59 +0000

    start to implement the new module annuities.py

--- annuities.py
+++ annuities.py
@@ -34,9 +34,10 @@ def annuity_x(mt, x, x_first, x_last, i=None, g=.0, m=1, method='udd'):
 
 
 # life annuities 1 head
-def ax(mt, x, i=None, g=0, m=1, defer=0, method='udd'):
+# immediate
+def ax(mt, x, i=None, g=0, m=1, method='udd'):
     '''
-    computes a whole life annuity immediate paying while both lives are alive
+    computes a whole life annuity immediate
     :param mt: table for life x
     :param x: age x
     :param i: technical interest rate (flat rate) in percentage, e.g., 2 for 2%
@@ -50,9 +51,59 @@ def ax(mt, x, i=None, g=0, m=1, defer=0, method='udd'):
     return annuity_x(mt=mt, x=x, x_first=x + 1 / m, x_last=mt.w, i=i, g=g, m=m, method=method)
 
 
-def aax(mt, x, i=None, g=0, m=1, defer=0, method='udd'):
+def t_ax(mt, x, i=None, g=0, m=1, defer=0, method='udd'):
     '''
-    computes a whole life annuity due paying while both lives are alive
+    computes a whole life annuity immediate
+    :param mt: table for life x
+    :param x: age x
+    :param i: technical interest rate (flat rate) in percentage, e.g., 2 for 2%
+    :param g: growth rate (flat rate) in percentage, e.g., 2 for 2%
+    :param m: frequency of payments per unit of interest rate quoted
+    :param defer: deferment period
+    :param method: the method to approximate the fractional periods
+    :return: the actuarial present value
+    '''
+
+    return annuity_x(mt=mt, x=x, x_first=x + 1 / m + defer, x_last=mt.w, i=i, g=g, m=m, method=method)
+
+
+def nax(mt, x, n, i=None, g=0, m=1, method='udd'):
+    '''
+    Return the actuarial present value of a (immediate) temporal (term certain) annuity: n-year temporary
+        life annuity-late. Payable 'm' per year at the ends of the period
+    :param mt: table for life x
+    :param x: age x
+    :param i: technical interest rate (flat rate) in percentage, e.g., 2 for 2%
+    :param g: growth rate (flat rate) in percentage, e.g., 2 for 2%
+    :param m: frequency of payments per unit of interest rate quoted
+    :param method: the method to approximate the fractional periods
+    :return: the actuarial present value
+    '''
+
+    return annuity_x(mt=mt, x=x, x_first=x + 1 / m, x_last=x + n, i=i, g=g, m=m, method=method)
+
+
+def t_nax(mt, x, n, i=None, g=0, m=1, defer=0, method='udd'):
+    '''
+    Return the actuarial present value of a (immediate) temporal (term certain) annuity: n-year temporary
+        life annuity-late. Payable 'm' per year at the ends of the period
+    :param mt: table for life x
+    :param x: age x
+    :param i: technical interest rate (flat rate) in percentage, e.g., 2 for 2%
+    :param g: growth rate (flat rate) in percentage, e.g., 2 for 2%
+    :param m: frequency of payments per unit of interest rate quoted
+    :param defer: deferment period
+    :param method: the method to approximate the fractional periods
+    :return: the actuarial present value
+    '''
+
+    return annuity_x(mt=mt, x=x, x_first=x + 1 / m + defer, x_last=x + n + defer, i=i, g=g, m=m, method=method)
+
+
+# due
+def aax(mt, x, i=None, g=0, m=1, method='udd'):
+    '''
+    computes a whole life annuity due
     :param mt: table for life x
     :param x: age x
     :param i: technical interest rate (flat rate) in percentage, e.g., 2 for 2%
@@ -64,69 +115,3 @@ def aax(mt, x, i=None, g=0, m=1, defer=0, method='udd'):
     '''
 
     return annuity_x(mt=mt, x=x, x_first=x, x_last=mt.w, i=i, g=g, m=m, method=method)
-
-
-# life annuities 2 heads
-def axy(mtx, mty, x, y, i=None, g=0, m=1, defer=0, method='udd'): # todo: recalculate besed on the generic anuuity but now for 2 heads
-    '''
-    computes a whole life annuity immediate paying while both lives are alive
-    :param mtx: table for life x
-    :param mty: table for life y
-    :param x: age x
-    :param y: age y
-    :param i: technical interest rate (flat rate) in percentage, e.g., 2 for 2%
-    :param g: growth rate (flat rate) in percentage, e.g., 2 for 2%
-    :param m: frequency of payments per unit of interest rate quoted
-    :param defer: deferment period
-    :param method: the method to approximate the fractional periods
-    :return: the actuarial present value
-    '''
-    years_to_wx = mtx.w - (x + defer) + 1
-    years_to_wy = mty.w - (y + defer) + 1
-    years_to_end = np.max(np.min((years_to_wx, years_to_wy)), 0)
-    i = i / 100
-    g = g / 100
-    d = float((1 + g) / (1 + i))
-    if years_to_end == 0:  # at least one will die before the end of the period
-        return .0
-    number_of_payments = int(years_to_end * m - 1)
-    # starts paying one period after the deferment
-    payments_instants = np.linspace(defer + 1 / m, years_to_end - 1, number_of_payments)
-    instalments = [mtx.tpx(x, t=t, method=method) * mty.tpx(y, t=t, method=method) *
-                   np.power(d, t) for t in payments_instants]
-    instalments = np.array(instalments) / np.power(1 + g, defer) / m
-
-    return np.sum(instalments)
-
-
-def aaxy(mtx, mty, x, y, i=None, g=0, m=1, defer=0, method='udd'):
-    '''
-    computes a whole life annuity due paying while both lives are alive
-    :param mtx: table for life x
-    :param mty: table for life y
-    :param x: age x
-    :param y: age y
-    :param i: technical interest rate (flat rate) in percentage, e.g., 2 for 2%
-    :param g: growth rate (flat rate) in percentage, e.g., 2 for 2%
-    :param m: frequency of payments per unit of interest rate quoted
-    :param defer: deferment period
-    :param method: the method to approximate the fractional periods
-    :return: the actuarial present value
-    '''
-
-    years_to_wx = mtx.w - (x + defer) + 1
-    years_to_wy = mty.w - (y + defer) + 1
-    years_to_end = np.max(np.min((years_to_wx, years_to_wy)), 0)
-    i = i / 100
-    g = g / 100
-    d = float((1 + g) / (1 + i))
-    if years_to_end == 0:  # at least one will die before the end of the period
-        return .0
-    number_of_payments = int(years_to_end * m)
-    # starts paying immediately after the deferment
-    payments_instants = np.linspace(defer + 1 / m * 0, years_to_end, number_of_payments)
-    instalments = [mtx.tpx(x, t=t, method=method) * mty.tpx(y, t=t, method=method) *
-                   np.power(d, t) for t in payments_instants]
-    instalments = np.array(instalments) / np.power(1 + g, defer) / m
-
-    return np.sum(instalments)
