233fbd5821bbd3973facfafe2b1f79790e8e489e
parcr <pedro.cortereal@magentakoncept.com>
2020-02-12 13:15:36 +0000

    test age

--- essay/essay_age.py
+++ essay/essay_age.py
@@ -5,6 +5,7 @@ import numpy as np
 import datetime
 import calendar
 from dateutil.relativedelta import relativedelta
+import pandas as pd
 
 sd = '1956-12-24'
 sd2 = '1841-03-02'
@@ -18,6 +19,11 @@ print(f"Age for {a} is {a.age_f()[0]} years, {a.age_f()[1]} months and {a.age_f(
 
 d1 = datetime.date(1968, 2, 14)
 d2 = datetime.date.today()
+print(relativedelta(d2,d1))
 a = age.Age(d1, d2)
 print(f"Age for {a} is {a.age_f()[0]} years, {a.age_f()[1]} months and {a.age_f()[2]} days,"
       f" that is, {a.age_f()[3]} years")
+
+d1 = "1968-02-14"
+d2 = datetime.date.today()
+print((pd.to_datetime(d2) - pd.to_datetime(d1)))
