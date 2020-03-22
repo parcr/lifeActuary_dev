52de437f9d3921857463a56a376017f3eac779f7
parcr <pedro.cortereal@magentakoncept.com>
2020-03-22 18:23:55 +0000

    update disability_tables.py

---
+++ essay/read_disability_table.py
@@ -0,0 +1,13 @@
+__author__ = "PedroCR"
+
+from disability_tables import disability_tables as dt
+from turnover_tables import turnover_tables as tt
+import mortality_table as mt
+
+ekv80 = dt.EVK_80
+ekv80_70 = dt.EVK_80_ext_70
+pcr1 = tt.pcr_turnover
+pcr2 = tt.pcr_turnover_65
+
+dt_ekv80 = mt.MortalityTable(mt=ekv80)
+tt_pcr = mt.MortalityTable(mt=pcr1)
