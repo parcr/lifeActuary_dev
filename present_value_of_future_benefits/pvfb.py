e93e02af701d4b8090feb3edcd0cb6645c9c2164
pedrocr <pedro.cortereal@magentakoncept.com>
2020-03-29 15:28:56 +0100

    start of pvfb.py

--- present_value_of_future_benefits/pvfb.py
+++ present_value_of_future_benefits/pvfb.py
@@ -3,3 +3,9 @@ __author__ = "PedroCR"
 import numpy as np
 import age
 
+
+class PVFB:
+    def __init__(self, date_of_valuation, date_of_birth,
+                 date_of_entry, age_of_term_cost, multi_table=None, decrement=None, i=None,
+                 age_first_instalment=None, age_last_instalment=None, age_first_payment=None):
+        pass
