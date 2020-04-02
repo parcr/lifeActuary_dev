9150a0387e7ca6ca19b29c5605ba0e0193e449e1
parcr <pedro.cortereal@magentakoncept.com>
2020-04-02 14:23:10 +0100

    create present_value_of_future_benefits and pvfb.py

--- essay/essay_manage_population.py
+++ essay/essay_manage_population.py
@@ -6,6 +6,7 @@ from turnover_tables import turnover_tables as tt
 import mortality_table as mt
 from multidecrement_table import MultiDecrementTable as mdt
 import numpy as np
+import matplotlib.pyplot as plt
 
 soa_TV7377 = rst.SoaTable('../soa_tables/TV7377.xml')
 soa_GRF95 = rst.SoaTable('../soa_tables/GRF95.xml')
@@ -47,7 +48,7 @@ def transition_matrix_1(transition_info, x):
     c = len(transition_info)
     r = len(transition_info[0])
     mat = np.zeros((r, c))
-    control = np.zeros((r, 1))
+    control = np.zeros((r))
     for r_i, r in enumerate(transition_info):
         for c_i, c in enumerate(r):
             if type(transition_info[r_i][c_i]) == tuple:
@@ -57,8 +58,49 @@ def transition_matrix_1(transition_info, x):
                     mat[r_i][c_i] = transition_info[r_i][c_i][0].tqx(x=x, t=1, method='udd')
             else:
                 mat[r_i][c_i] = transition_info[r_i][c_i]
-        control[r_i][0] = sum(mat[r_i])
+        control[r_i] = sum(mat[r_i])  # todo: pass control to log when sum of rows are different of 1
     return mat
 
 
+def transition_matrix_n(transition_info, x1, x2, init_state=None):
+    if x2 <= x1: return None
+    c = len(transition_info)
+    r = len(transition_info[0])
+    mat = np.identity(c)
+    if init_state is None:
+        init_state = np.zeros((r, 1))
+        final_state = init_state.copy()
+        init_state[0][0] = 1
+
+    control = []
+    state_path = []
+    for x in range(x1, x2):
+        mat_1 = transition_matrix_1(transition_info, x)
+        mat = np.dot(mat, mat_1)
+        control.append(mat.sum(axis=1))
+        final_state = np.dot(mat.transpose(), init_state)
+        state_path.append(final_state.flatten().tolist())
+    return mat, state_path
+
+
 mat = transition_matrix_1(transition_info, x=45)
+mat_n = transition_matrix_n(transition_info, 45, 106, np.array([100000, 0, 0, 0]))
+init_pop = np.array((1000, 0, 0, 0))
+final_pop = np.dot(mat_n[0].transpose(), init_pop)
+
+# if we want to create a dictionary
+keys = ['active', 'disable', 'withdraw', 'death']
+final_pop_dict = dict()
+state_path_to_plot = np.array(mat_n[1]).transpose().tolist()
+for k_i, k in enumerate(keys):
+    final_pop_dict[k] = state_path_to_plot[k_i]
+
+# plot a bar chart
+ages = list(range(len(mat_n[1])))
+bottom = np.zeros(len(mat_n[1]))
+for k, v in final_pop_dict.items():
+    plt.bar(ages, v, bottom=bottom)
+    bottom += v
+plt.legend(final_pop_dict.keys(), loc=2)
+plt.xlabel('Year')
+plt.ylabel('Population Path')
