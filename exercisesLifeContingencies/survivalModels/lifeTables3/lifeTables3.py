3088448020f652025f864594841d5785ed9dbb3a
pedro <pedroarccortereal@gmail.com>
2021-04-23 10:26:06 +0100

    prepare exercise lifeTables3.py

--- exercisesLifeContingencies/survivalModels/lifeTables3/lifeTables3.py
+++ exercisesLifeContingencies/survivalModels/lifeTables3/lifeTables3.py
@@ -1,8 +1,12 @@
 import numpy as np
+import os
+import sys
 
 from soa_tables import read_soa_table_xml as rst
 from essential_life import mortality_table, commutation_table
 import matplotlib.pyplot as plt
+
+this_py = os.path.split(sys.argv[0])[-1][:-3]
 
 
 def parse_table_name(name):
@@ -10,7 +14,7 @@ def parse_table_name(name):
 
 
 table_names = ['TV7377', 'GRF95', 'GRM95']
-mt_lst = [rst.SoaTable('../../soa_tables/' + name + '.xml') for name in table_names]
+mt_lst = [rst.SoaTable('../../../soa_tables/' + name + '.xml') for name in table_names]
 lt_lst = [mortality_table.MortalityTable(mt=mt.table_qx) for mt in mt_lst]
 ct_lst = [commutation_table.CommutationFunctions(i=1.4, g=1, mt=mt.table_qx) for mt in mt_lst]
 
@@ -29,4 +33,5 @@ plt.ylabel(r'${e}_{x}+1/2$')
 plt.title('Complete Expectation of Life')
 plt.grid(b=True, which='both', axis='both', color='grey', linestyle='-', linewidth=.1)
 plt.legend()
+plt.savefig(this_py + '.eps', format='eps', dpi=3600)
 plt.show()
