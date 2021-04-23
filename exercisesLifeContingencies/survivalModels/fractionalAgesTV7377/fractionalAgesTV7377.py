d8f744e1b750486a7f739ef20c0e5f3fe63bf62c
pedro <pedroarccortereal@gmail.com>
2021-04-23 19:24:32 +0100

    prepare exercise lifeTables3.py

---
+++ exercisesLifeContingencies/survivalModels/fractionalAgesTV7377/fractionalAgesTV7377.py
@@ -0,0 +1,36 @@
+import numpy as np
+import os
+import sys
+
+from soa_tables import read_soa_table_xml as rst
+from essential_life import mortality_table, commutation_table
+import matplotlib.pyplot as plt
+
+this_py = os.path.split(sys.argv[0])[-1][:-3]
+
+
+def parse_table_name(name):
+    return name.replace(' ', '').replace('/', '')
+
+
+table_names = ['TV7377', 'GRF95', 'GRM95']
+mt_lst = [rst.SoaTable('../../../soa_tables/' + name + '.xml') for name in table_names]
+lt_lst = [mortality_table.MortalityTable(mt=mt.table_qx) for mt in mt_lst]
+ct_lst = [commutation_table.CommutationFunctions(i=1.4, g=1, mt=mt.table_qx) for mt in mt_lst]
+
+'''
+Exercises
+'''
+mt = lt_lst[0]
+a = mt.tqx(x=52.4, t=0.2, method='udd')
+print(a)
+b = mt.tqx(x=52.4, t=0.2, method='cfm')
+print(b)
+c = mt.tpx(x=52.4, t=5.7, method='udd')
+print(c)
+d = mt.tpx(x=52.4, t=5.7, method='cfm')
+print(d)
+e = mt.t_nqx(x=52.4, t=3.2, n=2.5, method='udd')
+print(e)
+f = mt.t_nqx(x=52.4, t=3.2, n=2.5, method='cfm')
+print(f)
