47add7e2e01d2edf91f0a8f5e8c7e3d242a72090
pedro <pedroarccortereal@gmail.com>
2021-05-07 19:46:49 +0100

    prepare exercise lifeTables3.py

--- exercisesLifeContingencies/survivalModels/lifeTables3/lifeTables3.py
+++ exercisesLifeContingencies/survivalModels/lifeTables3/lifeTables3.py
@@ -16,13 +16,18 @@ def parse_table_name(name):
 table_names = ['TV7377', 'GRF95', 'GRM95']
 mt_lst = [rst.SoaTable('../../../soa_tables/' + name + '.xml') for name in table_names]
 lt_lst = [mortality_table.MortalityTable(mt=mt.table_qx) for mt in mt_lst]
-ct_lst = [commutation_table.CommutationFunctions(i=1.4, g=1, mt=mt.table_qx) for mt in mt_lst]
+ct_lst = [commutation_table.CommutationFunctions(i=4, g=0, mt=mt.table_qx) for mt in mt_lst]
 
 for idx, lt in enumerate(lt_lst):
     name = parse_table_name(mt_lst[idx].name)
     lt.df_life_table().to_excel(excel_writer=name + '.xlsx', sheet_name=name,
                                 index=False, freeze_panes=(1, 1))
+    ct_lst[idx].df_commutation_table().to_excel(excel_writer=name + '_comm' + '.xlsx', sheet_name=name,
+                                                index=False, freeze_panes=(1, 1))
 
+'''
+Plot ex
+'''
 fig, axes = plt.subplots()
 for idx, lt in enumerate(lt_lst):
     ages = np.arange(0, lt.w + 1)
@@ -34,4 +39,20 @@ plt.title('Complete Expectation of Life')
 plt.grid(b=True, which='both', axis='both', color='grey', linestyle='-', linewidth=.1)
 plt.legend()
 plt.savefig(this_py + '.eps', format='eps', dpi=3600)
+plt.show()
+
+'''
+Plot ln(Dx)
+'''
+fig, axes = plt.subplots()
+for idx, lt in enumerate(ct_lst):
+    ages = np.arange(0, lt.w + 1)
+    plt.plot(ages, np.log(lt.Dx), label=table_names[idx])
+
+plt.xlabel(r'$x$')
+plt.ylabel(r'$ln(D_x)$')
+plt.title(r'ln(D_x)')
+plt.grid(b=True, which='both', axis='both', color='grey', linestyle='-', linewidth=.1)
+plt.legend()
+plt.savefig(this_py + '_comm1' + '.eps', format='eps', dpi=3600)
 plt.show()
