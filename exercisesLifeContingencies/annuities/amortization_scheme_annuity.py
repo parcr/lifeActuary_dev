3657b637f1cb212281adb825fe3d38aaeec9347e
pedro <pedroarccortereal@gmail.com>
2021-06-10 19:40:04 +0100

    amortization_scheme_annuity.py

--- exercisesLifeContingencies/annuities/amortization_scheme_annuity.py
+++ exercisesLifeContingencies/annuities/amortization_scheme_annuity.py
@@ -15,26 +15,24 @@ lx = 1000
 frequency = 1
 first_instalment_at = 5
 number_of_instalments = 1000
+premium_instalments = 5
 instalment = 10000
-x0 = 50
-renda = ct.aax(x=x0)
-# todo: change here to become generic; fund_0 = ct.aax(x=x0) * instalment * lx
-dict_annuity = {'x': x0, 'm': 1, 'defer': first_instalment_at}
-fund_0 = ct.t_aax(x=dict_annuity['x'], m=dict_annuity['m'], defer=dict_annuity['defer']) * instalment * lx
-premium_instalments = 1
-renda_aux = ct.naax(x=x0, n=premium_instalments)
-premium1 = fund_0 / lx / renda_aux
+dict_annuity = {'x': 50, 'n': 1000, 'm': 1, 'defer': first_instalment_at}
+renda_premium = ct.naax(x=dict_annuity['x'], n=premium_instalments)
+full_liability = ct.t_naax(x=dict_annuity['x'], n=dict_annuity['n'], m=dict_annuity['m'], defer=dict_annuity['defer']) * \
+                 instalment * lx
+premium1 = full_liability / lx / renda_premium
 
 i = interest_rate / 100
 dict_liability = {'t': [], 'x': [], 'tpx': [], 'premium1': [], 'claim1': [],
                   'fund1': [], 'lx': [], 'premium': [], 'claim': [], 'fund': []}
 
-dict_reserves = {'t': [], 'x': [], 'insured': [], 'insurer': [], 'reserve': [], 'fund': []}
+dict_reserves = {'t': [], 'x': [], 'insured': [], 'insurer': [], 'reserve': []}
 
-for idx, x in enumerate(range(x0, lt.w + 1 + 1)):
+for idx, x in enumerate(range(dict_annuity['x'], lt.w + 1 + 1)):
     dict_liability['t'].append(idx)
     dict_liability['x'].append(x)
-    dict_liability['tpx'].append(lt.tpx(x=x0, t=idx))
+    dict_liability['tpx'].append(lt.tpx(x=dict_annuity['x'], t=idx))
     if idx <= premium_instalments - 1:
         dict_liability['premium1'].append(dict_liability['tpx'][-1] * premium1)
     else:
@@ -44,9 +42,10 @@ for idx, x in enumerate(range(x0, lt.w + 1 + 1)):
     else:
         dict_liability['claim1'].append(0)
     if idx == 0:
-        dict_liability['fund1'].append(fund_0 / lx - dict_liability['claim1'][-1])
+        dict_liability['fund1'].append(premium1 - dict_liability['claim1'][-1])
     else:
-        dict_liability['fund1'].append(dict_liability['fund1'][-1] * (1 + i) - dict_liability['claim1'][-1])
+        dict_liability['fund1'].append(dict_liability['fund1'][-1] * (1 + i) -
+                                       dict_liability['claim1'][-1] + dict_liability['premium1'][-1])
     dict_liability['lx'].append(lx * dict_liability['tpx'][-1])
     dict_liability['premium'].append(dict_liability['premium1'][-1] * lx)
     dict_liability['claim'].append(dict_liability['claim1'][-1] * lx)
@@ -54,15 +53,20 @@ for idx, x in enumerate(range(x0, lt.w + 1 + 1)):
 
     # reserves
     if idx <= premium_instalments - 1:
-        renda_aux = ct.naax(x=x0, n=premium_instalments - idx)
-        dict_reserves['insured'].append(dict_liability['lx'][-1] * premium1 * renda_aux)
+        renda_premium_aux = ct.naax(x=dict_annuity['x'] + idx, n=premium_instalments - idx)
+        dict_reserves['insured'].append(dict_liability['lx'][-1] * premium1 * renda_premium_aux)
     else:
         dict_reserves['insured'].append(.0)
-    renda_aux_2 = ct.t_aax(x=dict_annuity['x'] + idx, m=dict_annuity['m'],
-                           defer=max(dict_annuity['defer'] - idx, 0))  # ct.aax(x=x0 + idx)
+    if idx < dict_annuity['defer']:  # we haven't started paying
+        renda_aux_2 = ct.t_naax(x=dict_annuity['x'] + idx, n=dict_annuity['n'], m=dict_annuity['m'],
+                                defer=dict_annuity['defer'] - idx)
+    else:
+        renda_aux_2 = ct.t_naax(x=dict_annuity['x'] + idx, n=dict_annuity['n'] - (idx - dict_annuity['defer']),
+                                m=dict_annuity['m'], defer=0)
     dict_reserves['insurer'].append(dict_liability['lx'][-1] * renda_aux_2 * instalment)
     dict_reserves['reserve'].append(dict_reserves['insurer'][-1] - dict_reserves['insured'][-1])
-    dict_reserves['fund'].append(dict_reserves['reserve'][-1] - dict_liability['claim'][-1])
+    # value of the fund, immediately after all due payments
+    # dict_reserves['fund'].append(dict_reserves['reserve'][-1] - dict_liability['claim'][-1])
 
 dict_reserves['t'] = dict_liability['t']
 dict_reserves['x'] = dict_liability['x']
