07d7fca9b66fed7b9a7cb1c71764be3f6084ec8b
pedro <pedroarccortereal@gmail.com>
2021-07-28 17:29:18 +0100

    test life insurance

--- exercisesLifeContingencies/lifeInsurance/makeham_law_lifeInsurance_compare_var_endow.py
+++ exercisesLifeContingencies/lifeInsurance/makeham_law_lifeInsurance_compare_var_endow.py
@@ -14,7 +14,7 @@ ages = np.arange(start=20, stop=100 + 20, step=20)
 
 interest_rate = 5
 interest_rate_2 = ((1 + interest_rate / 100) ** 2 - 1) * 100
-capital = 1
+capital = 100000
 
 '''
 Compute Life Table
@@ -36,8 +36,8 @@ Ax_comm = [ct.nAx(x=x, n=10) for x in ages]
 Ax_2_comm = [ct_2.nAx(x, n=10) for x in ages]
 Ex_comm = [ct.nEx(x=x, n=10) for x in ages]
 Ex_2_comm = [ct_2.nEx(x, n=10) for x in ages]
-AEx_comm = [Ax_comm[j]*capital + Ex_comm[j]*capital for j in range(len(ages))]
-AEx_comm_compare = [ct.nAEx(x=x, n=10)*capital for x in ages]
+AEx_comm = [Ax_comm[j] * capital + Ex_comm[j] * capital for j in range(len(ages))]
+AEx_comm_compare = [ct.nAEx(x=x, n=10) * capital for x in ages]
 AEx_comm_error = (np.array(AEx_comm) - np.array(AEx_comm_compare))
 
 Ax_comm_var = [Ax_2_comm[j] - np.power(Ax_comm[j], 2) for j in range(len(ages))]
@@ -56,14 +56,25 @@ Ax_4 = [mml.life_insurance(x=x, interest_rate=interest_rate, age_first_instalmen
 Ax_2_4 = [
     mml.life_insurance(x=x, interest_rate=interest_rate_2, age_first_instalment=x, terms=term, fraction=4, w=129)
     for x in ages]
-Ax_4_std_dev = [np.power(Ax_2_4[j] - np.power(Ax_4[j], 2), .5) * capital for j in range(len(ages))]
+Ax_4_var = [Ax_2_4[j] - np.power(Ax_4[j], 2) for j in range(len(ages))]
+AEx_4 = [Ax_4[j] * capital + Ex_comm[j] * capital for j in range(len(ages))]
+Ax_Ex_4_cov = [Ax_4[j] * Ex_comm[j] for j in range(len(ages))]
+AEx_4_std_dev = [np.sqrt(Ax_4_var[j] + Ex_comm_var[j] - 2 * Ax_Ex_4_cov[j]) * capital for j in
+                 range(len(ages))]
+
+
+
 
 Ax_12 = [mml.life_insurance(x=x, interest_rate=interest_rate, age_first_instalment=x, terms=term, fraction=12, w=129)
          for x in ages]
 Ax_2_12 = [
     mml.life_insurance(x=x, interest_rate=interest_rate_2, age_first_instalment=x, terms=term, fraction=12, w=129)
     for x in ages]
-Ax_12_std_dev = [np.power(Ax_2_12[j] - np.power(Ax_12[j], 2), .5) * capital for j in range(len(ages))]
+Ax_12_var = [Ax_2_12[j] - np.power(Ax_12[j], 2) for j in range(len(ages))]
+AEx_12 = [Ax_12[j] * capital + Ex_comm[j] * capital for j in range(len(ages))]
+Ax_Ex_12_cov = [Ax_12[j] * Ex_comm[j] for j in range(len(ages))]
+AEx_12_std_dev = [np.sqrt(Ax_12_var[j] + Ex_comm_var[j] - 2 * Ax_Ex_12_cov[j]) * capital for j in
+                  range(len(ages))]
 
 '''
 compute Whole Life Insurance for fraction ages using the survival function to compute 
@@ -72,7 +83,11 @@ the probabilities of non integer ages
 
 Ax_cont = [mml.Ax(x=x, interest_rate=interest_rate, n=term) for x in ages]
 Ax_2_cont = [mml.Ax(x=x, interest_rate=interest_rate_2, n=term) for x in ages]
-Ax_cont_std_dev = [np.power(Ax_2_cont[j] - np.power(Ax_cont[j], 2), .5) * capital for j in range(len(ages))]
+Ax_cont_var = [Ax_2_cont[j] - np.power(Ax_cont[j], 2) for j in range(len(ages))]
+AEx_cont = [Ax_cont[j] * capital + Ex_comm[j] * capital for j in range(len(ages))]
+Ax_Ex_cont_cov = [Ax_cont[j] * Ex_comm[j] for j in range(len(ages))]
+AEx_cont_std_dev = [np.sqrt(Ax_cont_var[j] + Ex_comm_var[j] - 2 * Ax_Ex_cont_cov[j]) * capital for j in
+                    range(len(ages))]
 
 '''
 compute Whole Life Insurance for fraction ages using the using Commutation Functions and the usual approximations
@@ -92,33 +107,41 @@ factor_1 = interest_rate / effective_nominal_interest_rate(interest_rate=interes
 factor_2 = interest_rate_2 / effective_nominal_interest_rate(interest_rate=interest_rate_2, freq=4)
 Ax_comm_4 = np.array(Ax_comm) * factor_1
 Ax_2_comm_4 = np.array(Ax_2_comm) * factor_2
-Ax_comm_4_std_dev = np.sqrt(Ax_2_comm_4 - np.power(Ax_comm_4, 2)) * capital
+Ax_comm_4_var = Ax_2_comm_4 - np.power(Ax_comm_4, 2)
+AEx_comm_4 = [Ax_comm_4[j] * capital + Ex_comm[j] * capital for j in range(len(ages))]
+Ax_Ex_comm_4_cov = [Ax_comm_4[j] * Ex_comm[j] for j in range(len(ages))]
+AEx_comm_4_std_dev = [np.sqrt(Ax_comm_4_var[j] + Ex_comm_var[j] - 2 * Ax_Ex_comm_4_cov[j]) * capital for j in
+                      range(len(ages))]
 
 factor_1 = interest_rate / effective_nominal_interest_rate(interest_rate=interest_rate, freq=12)
 factor_2 = interest_rate_2 / effective_nominal_interest_rate(interest_rate=interest_rate_2, freq=12)
 Ax_comm_12 = np.array(Ax_comm) * factor_1
 Ax_2_comm_12 = np.array(Ax_2_comm) * factor_2
-Ax_comm_12_std_dev = np.sqrt(Ax_2_comm_12 - np.power(Ax_comm_12, 2)) * capital
+Ax_comm_12_var = Ax_2_comm_12 - np.power(Ax_comm_12, 2)
+AEx_comm_12 = [Ax_comm_12[j] * capital + Ex_comm[j] * capital for j in range(len(ages))]
+Ax_Ex_comm_12_cov = [Ax_comm_12[j] * Ex_comm[j] for j in range(len(ages))]
+AEx_comm_12_std_dev = [np.sqrt(Ax_comm_12_var[j] + Ex_comm_var[j] - 2 * Ax_Ex_comm_12_cov[j]) * capital for j in
+                       range(len(ages))]
 
 '''
 Prepare the data frame
 '''
 
-rounded_to = 2
+rounded_to = 6
 Ax_compare_df = pd.DataFrame({'Age': ages,
-                              'Ax_comm': np.round(np.array(Ax_comm) * capital, rounded_to),
-                              'Ax_comm_std_dev': np.round(Ax_comm_std_dev, rounded_to),
-                              'Ax_4': np.round(np.array(Ax_4) * capital, rounded_to),
-                              'Ax_4_std_dev': np.round(Ax_4_std_dev, rounded_to),
-                              'Ax_12': np.round(np.array(Ax_12) * capital, rounded_to),
-                              'Ax_12_std_dev': np.round(Ax_12_std_dev, rounded_to),
-                              'Ax_cont': np.round(np.array(Ax_cont) * capital, rounded_to),
-                              'Ax_cont_std_dev': np.round(Ax_cont_std_dev, rounded_to),
-                              'Ax_comm_4': np.round(np.array(Ax_comm_4) * capital, rounded_to),
-                              'Ax_comm_4_std_dev': np.round(Ax_comm_4_std_dev, rounded_to),
-                              'Ax_comm_12': np.round(np.array(Ax_comm_12) * capital, rounded_to),
-                              'Ax_comm_12_std_dev': np.round(Ax_comm_12_std_dev, rounded_to),
+                              'AEx_comm': np.round(np.array(AEx_comm), rounded_to),
+                              'AEx_comm_std_dev': np.round(AEx_comm_std_dev, rounded_to),
+                              'AEx_4': np.round(np.array(AEx_4), rounded_to),
+                              'AEx_4_std_dev': np.round(AEx_4_std_dev, rounded_to),
+                              'AEx_12': np.round(np.array(AEx_12), rounded_to),
+                              'AEx_12_std_dev': np.round(AEx_12_std_dev, rounded_to),
+                              'AEx_cont': np.round(np.array(AEx_cont), rounded_to),
+                              'AEx_cont_std_dev': np.round(AEx_cont_std_dev, rounded_to),
+                              'AEx_comm_4': np.round(np.array(AEx_comm_4), rounded_to),
+                              'AEx_comm_4_std_dev': np.round(AEx_comm_4_std_dev, rounded_to),
+                              'AEx_comm_12': np.round(np.array(AEx_comm_12), rounded_to),
+                              'AEx_comm_12_std_dev': np.round(AEx_comm_12_std_dev, rounded_to),
                               }
                              )
-Ax_compare_df.to_excel(excel_writer='Ax_compare_temp_df' + '.xlsx', sheet_name='Ax_compare_temp',
+Ax_compare_df.to_excel(excel_writer='AEx_compare_temp_df' + '.xlsx', sheet_name='AEx_compare_temp',
                        index=False, freeze_panes=(1, 1))
