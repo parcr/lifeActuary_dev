ddc3581d028d574b2db1a35ca6f967bbce289609
pedrocortereal <pedroarcortereal@gmail.com>
2021-12-31 15:45:03 +0000

    Prepare 2nd teste 2021_22

--- exercisesLifeContingencies/test2_2021_22/exercise_3.py
+++ exercisesLifeContingencies/test2_2021_22/exercise_3.py
@@ -24,11 +24,11 @@ def parse_table_name(name):
 
 
 table_names = ['TV7377', 'GRF95', 'GRM95']
-interest_rate = 3.8
+interest_rate = 4
 mt_lst = [rst.SoaTable('../../soa_tables/' + name + '.xml') for name in table_names]
 lt_lst = [mortality_table.MortalityTable(mt=mt.table_qx) for mt in mt_lst]
 ct_lst = [commutation_table.CommutationFunctions(i=interest_rate, g=0, mt=mt.table_qx) for mt in mt_lst]
-ages = np.linspace(start=20, stop=40, num=5, dtype=int)
+# ages = np.linspace(start=20, stop=40, num=5, dtype=int)
 
 '''
 Prepare the solution for Equal Instalments
@@ -36,9 +36,11 @@ Prepare the solution for Equal Instalments
 ages = np.linspace(start=35, stop=35, num=1, dtype=int)
 capital = 200000
 terms = 20
+
 ac = annuities_certain.Annuities_Certain(interest_rate=interest_rate, frequency=1)
 ac_certain = ac.annuity_immediate(terms=terms)
-equal_instalments_dict = {'table': [], 'x': [], 'annuity_certain': [], 'annuity': [], 'premium': []}
+equal_instalments_dict = {'table': [], 'x': [], 'annuity_certain': [], 'annuity': [], 'premium': [],
+                          'annuity_level': [], 'premium_leveled': []}
 
 for id_ct, ct in enumerate(ct_lst):
     for id_x, x in enumerate(ages):
@@ -47,7 +49,11 @@ for id_ct, ct in enumerate(ct_lst):
         equal_instalments_dict['annuity_certain'].append(ac_certain)
         annuity = ct.nax(x=x, n=terms, m=1)
         equal_instalments_dict['annuity'].append(annuity)
-        equal_instalments_dict['premium'].append(capital - capital * annuity / ac_certain)
+        premium = capital - capital * annuity / ac_certain
+        equal_instalments_dict['premium'].append(premium)
+        annuity_level = ct.naax(x=x, n=terms, m=1)
+        equal_instalments_dict['annuity_level'].append(annuity_level)
+        equal_instalments_dict['premium_leveled'].append(premium / annuity_level)
 
 equal_instalments_df = pd.DataFrame(equal_instalments_dict)
 equal_instalments_df.to_excel(excel_writer='equal_instalments' + '.xlsx',
@@ -58,7 +64,8 @@ equal_instalments_df.to_excel(excel_writer='equal_instalments' + '.xlsx',
 Prepare the solution for Equal Amortizations
 '''
 
-equal_amortizations_dict = {'table': [], 'x': [], 'life_ins': [], 'life_ins_inc': [], 'premium': []}
+equal_amortizations_dict = {'table': [], 'x': [], 'life_ins': [], 'life_ins_inc': [], 'premium': [],
+                            'annuity_level': [], 'premium_leveled': []}
 
 for id_ct, ct in enumerate(ct_lst):
     for id_x, x in enumerate(ages):
@@ -68,10 +75,26 @@ for id_ct, ct in enumerate(ct_lst):
         equal_amortizations_dict['life_ins'].append(life_ins)
         life_ins_inc = ct.nIAx(x=x, n=terms)
         equal_amortizations_dict['life_ins_inc'].append(life_ins_inc)
-        equal_amortizations_dict['premium'].append(
-            capital * (1 + interest_rate / 100) / terms * ((terms + 1) * life_ins - life_ins_inc))
+        premium = capital * (1 + interest_rate / 100) / terms * ((terms + 1) * life_ins - life_ins_inc)
+        equal_amortizations_dict['premium'].append(premium)
+        annuity_level = ct.naax(x=x, n=terms, m=1)
+        equal_amortizations_dict['annuity_level'].append(annuity_level)
+        equal_amortizations_dict['premium_leveled'].append(premium / annuity_level)
 
 equal_amortizations_df = pd.DataFrame(equal_amortizations_dict)
 equal_amortizations_df.to_excel(excel_writer='equal_amortizations' + '.xlsx',
                                 sheet_name='equal_amortizations',
                                 index=False, freeze_panes=(1, 1))
+
+'''
+\item The loss for the insurer the lady dies immediately after paying the $10^{th}$ instalment.
+'''
+annuity_terms = 10
+annuities_certain_m = ac.annuity_immediate(terms=annuity_terms)
+annuities_certain_n_m = ac.annuity_immediate(terms=terms - annuity_terms)
+
+loan_balance=capital/ac_certain*annuities_certain_n_m
+premiums_paid=equal_instalments_dict['premium_leveled'][0]*annuities_certain_m*(1+interest_rate/100)**annuity_terms
+print('loan balance=', loan_balance)
+print('premiums paid=', premiums_paid)
+print('Loss=', loan_balance-premiums_paid)
