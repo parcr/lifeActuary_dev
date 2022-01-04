61ef3363cd135945ceab3ac4d31268512fa4e4e7
pedrocortereal <pedroarcortereal@gmail.com>
2022-01-04 11:02:07 +0000

    Prepare 2nd teste 2021_22

--- exercisesLifeContingencies/netPremiumReserves/tli_55_1_net_reserves.py
+++ exercisesLifeContingencies/netPremiumReserves/tli_55_1_net_reserves.py
@@ -1,17 +1,19 @@
 from exercisesLifeContingencies.lifeInsurance import tli_55_1
 import pandas as pd
+import matplotlib.pyplot as plt
 
 '''
 Net premium reserves path
 '''
 reserves_dict = {'table': [], 'x': [], 'insurer': [], 'insured': [], 'reserve': []}
+ages = range(tli_55_1.x, tli_55_1.x + tli_55_1.term + 1)
 print('\n\n Net Premium reserves \n\n')
 for idx_clt, clt in enumerate(tli_55_1.ct_lst):
     premium_unit = tli_55_1.tli[idx_clt]
     premium_capital = tli_55_1.capital * premium_unit
     premium_unit_leveled = premium_unit / tli_55_1.tad[idx_clt]
     premium_leveled = premium_unit_leveled * tli_55_1.capital
-    for age in range(tli_55_1.x, tli_55_1.x+tli_55_1.term+1):
+    for age in ages:
         reserves_dict['table'].append(tli_55_1.table_names[idx_clt])
         reserves_dict['x'].append(age)
         insurer_liability = clt.nAx(x=age, n=tli_55_1.term - (age - tli_55_1.x)) * tli_55_1.capital
@@ -23,3 +25,11 @@ for idx_clt, clt in enumerate(tli_55_1.ct_lst):
 
 reserves_df = pd.DataFrame(reserves_dict)
 
+'''
+plot the reserves
+'''
+for idx_clt, clt in enumerate(tli_55_1.ct_lst):
+    plt.plot(ages, reserves_df.loc[reserves_df['table'] == tli_55_1.table_names[idx_clt]]['reserve'],
+             label=tli_55_1.table_names[idx_clt])
+
+plt.show()
