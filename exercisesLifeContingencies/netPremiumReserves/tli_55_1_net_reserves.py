1882c0b68f0fc54507576efddc209df614cfea5b
pedrocortereal <pedroarcortereal@gmail.com>
2022-01-07 17:17:04 +0000

    update net premium reserves

--- exercisesLifeContingencies/netPremiumReserves/tli_55_1_net_reserves.py
+++ exercisesLifeContingencies/netPremiumReserves/tli_55_1_net_reserves.py
@@ -5,7 +5,11 @@ import matplotlib.pyplot as plt
 '''
 Net premium reserves path
 '''
+l0 = 1000
 reserves_dict = {'table': [], 'x': [], 'insurer': [], 'insured': [], 'reserve': []}
+fund_dict = {'lx': [], 'claim': [], 'premium': [], 'fund': []}
+# expected reserves value, that is, considering the survivorship of the group
+expected_reserve_dict = {'insurer_exp': [], 'insured_exp': [], 'reserve_exp': []}
 ages = range(tli_55_1.x, tli_55_1.x + tli_55_1.term + 1)
 print('\n\n Net Premium reserves \n\n')
 for idx_clt, clt in enumerate(tli_55_1.ct_lst):
@@ -18,16 +22,41 @@ for idx_clt, clt in enumerate(tli_55_1.ct_lst):
         reserves_dict['x'].append(age)
         insurer_liability = clt.nAx(x=age, n=tli_55_1.term - (age - tli_55_1.x)) * tli_55_1.capital
         reserves_dict['insurer'].append(insurer_liability)
-        insured_liability = premium_leveled * clt.naax(x=age, n=tli_55_1.term_annuity - (age - tli_55_1.x))
+        tad = clt.naax(x=age, n=tli_55_1.term_annuity - (age - tli_55_1.x))
+        insured_liability = premium_leveled * tad
         reserves_dict['insured'].append(insured_liability)
         reserve = insurer_liability - insured_liability
         reserves_dict['reserve'].append(reserve)
 
+        prob_survival = clt.tpx(x=tli_55_1.x, t=age - tli_55_1.x)
+        lx = l0 * prob_survival
+        expected_reserve_dict['insurer_exp'].append(insurer_liability * lx)
+        expected_reserve_dict['insured_exp'].append(insured_liability * lx)
+        expected_reserve_dict['reserve_exp'].append(reserve * lx)
+
+        # fund # fund # fund # fund # fund # fund # fund # fund
+        fund_dict['lx'].append(lx)
+        qx_1 = clt.tqx(x=age - 1, t=1)
+        claim = 0
+        if (age > tli_55_1.x):
+            claim = l0 * clt.tpx(x=tli_55_1.x, t=age - tli_55_1.x - 1) * qx_1 * tli_55_1.capital
+        fund_dict['claim'].append(claim)
+        premium = 0
+        if tli_55_1.term_annuity - (age - tli_55_1.x) > 0:
+            premium = premium_leveled * lx
+        fund_dict['premium'].append(premium)
+        if age == tli_55_1.x:
+            fund = lx * premium_leveled
+        else:
+            fund = fund_dict['fund'][-1] * (1 + tli_55_1.interest_rate / 100) - claim + premium
+        fund_dict['fund'].append(fund)
+
 reserves_df = pd.DataFrame(reserves_dict)
+expected_reserve_df = pd.DataFrame(expected_reserve_dict)
+fund_df = pd.DataFrame(fund_dict)
 
 name = 'tli_55_1'
-reserves_df.to_excel(excel_writer=name + '_netReserves' + '.xlsx', sheet_name=name,
-                     index=False, freeze_panes=(1, 1))
+# reserves_df.to_excel(excel_writer=name + '_netReserves' + '.xlsx', sheet_name=name, index=False, freeze_panes=(1, 1))
 
 '''
 plot the reserves
