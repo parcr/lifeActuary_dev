d6a5e8843cadc1e874bfd3694c42870a18ac8d5e
pedrocortereal <pedroarcortereal@gmail.com>
2022-01-25 18:31:32 +0000

    update exam1_2021_22

--- exercisesLifeContingencies/exam1_2021_22/exercise_3.py
+++ exercisesLifeContingencies/exam1_2021_22/exercise_3.py
@@ -1,11 +1,12 @@
 '''
-\item Consider a \textbf{woman} aged 30 years old that purchases an Endowment with term at 65 years old. Considering $2\%$/annum as rate of interest and justifying all calculus, please determine:
+Considering \textbf{Makehams law of mortality} with $A=0.0004,B=1.2E-6$ and $c=1.08$ with a rate of interest of $1.8\%$/annum, determine:
 
 \begin{enumerate}
-\item For a capital of $500\:000$\euro, the monthly leveled risk premiums paid 25 years if in case of death the capital is paid at the moment of death.
+\item The 5 years monthly leveled risk premiums for an Endowment Insurance with term 10 years purchased by a life aged 50 if the payment in the event of death happens at the final of quarter of the death with capital underwritten $100\:000$\euro.
 
-\item For a capital of $500\:000$\euro, the single risk premium if in case of death the capital is paid at the end of the year but considering that the cover is deferred until the age of 40, keeping the term age at 65.
-\end{enumerate}
+\item The 15 years leveled risk premiums for a Whole Life Insurance purchased by a life aged 50, paying $200\:000$\euro\ at the moment of death.
+
+\item What is the reduction produced by the guarantee of $10$ terms, if a life 65 years old decide to swap a whole life annuity-due that pays $10\:000$\euro$\:$ per year for an annuity with this guarantee.
 '''
 
 import numpy as np
@@ -60,12 +61,39 @@ print('single_premium_capital:', round(single_premium_capital, 5))
 print('tad_12:', round(tad_12, 10))
 print('leveled_premium_capital:', round(leveled_premium_capital, 5))
 
+'''\item The 15 years leveled risk premiums for a Whole Life Insurance purchased by a life aged 50, paying 
+$200\:000$\euro\ at the moment of death.
+'''
+print('\n q2')
+endow = ct_lst[table_index].nAEx(x=x, n=term)
+v = 1 / (1 + interest_rate / 100)
+support = [v ** j for j in range(1, term + 1)]
+pmf = [ct_lst[table_index].t_nqx(x=x, t=j - 1, n=1) for j in range(1, term)]
+pmf.append(ct_lst[table_index].tpx(x=x, t=term - 1))
+cdf = np.cumsum(pmf)
+
+print('endowment:', round(endow, 10))
+print('support', support)
+print('pmf:', pmf)
+print('cdf', cdf)
+epv_lst = [pmf[j] * support[j] for j in range(0, term)]
+print('expected present value:', sum(epv_lst))
+print('test:', sum(epv_lst) - endow)
+cut_year = np.log(endow) / np.log(v)
+print('cut_year:', cut_year)
+print('cut_probability:', round(ct_lst[table_index].tpx(x=x, t=term - 1), 10))
+print('answer:', round(1 - ct_lst[table_index].tpx(x=x, t=term - 1), 10) * 100, '%', sep='')
+
+# another way
+probs_bool = [pmf[idx_v] for idx_v, v in enumerate(support) if v > endow]
+print('answer:', round(sum(probs_bool), 10) * 100, '%', sep='')
+
 '''
 \item For a capital of $500\:000$\euro, the single risk premium if in case of death the capital is paid at the 
 end of the year but considering that the cover is deferred until the age of 40, keeping the term age at 65.
 '''
 
-print('\n q2')
+print('\n q3')
 defer = 5
 term = term - defer
 deferment_factor = ct_lst[table_index].nEx(x=x, n=defer)
